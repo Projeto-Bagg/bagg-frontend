4732fccfa4763099b724c4fe3245ecde8bca5155
Felipe Brito <89111098+fefezoka@users.noreply.github.com>
2024-02-27 01:49:04 -0300

    misc: adicionando headers de segurança

--- next.config.js
+++ next.config.js
@@ -1,7 +1,52 @@
 const withNextIntl = require('next-intl/plugin')();
 
+const cspHeader = `
+    default-src 'self' http://localhost:3001 https://bagg-api.azurewebsites.net;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' http://localhost:3001 https://bagg-api.azurewebsites.net;
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https://baggstorage.blob.core.windows.net;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'microphone=(), geolocation=()',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
     config.module.rules.push(
